6a98437bcfbd1758049154ee6e18a1b80a15c2a8
skwasim777 <wasimsk6061@gmail.com>
2023-10-31 18:58:46 +0530

    getting and setting arraylist objects

---
+++ src/Collection/ArrayList/GetandSetArrayList.java
@@ -0,0 +1,19 @@
+package Collection.ArrayList;
+
+import java.util.ArrayList;
+
+public class GetandSetArrayList {
+	public static void main(String[] args) {
+		ArrayList<String> a = new ArrayList<>();
+		a.add("A");
+		a.add("B");
+		a.add("C");
+		a.add("D");
+		System.out.println(a.get(1));
+		a.set(1, "b");
+		for (String str : a) {
+			System.out.println(str);
+		}
+
+	}
+}
