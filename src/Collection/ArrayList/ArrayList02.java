5b8ceb036027ca7ac66ca11cbed5525d910f3664
skwasim777 <wasimsk6061@gmail.com>
2023-10-31 18:50:07 +0530

    iterating arraylist using iterator

---
+++ src/Collection/ArrayList/ArrayList02.java
@@ -0,0 +1,21 @@
+package Collection.ArrayList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayList02 {
+	public static void main(String[] args) {
+		ArrayList<String> a = new ArrayList<>();
+
+		a.add("Wasim");
+		
+		  a.add("Shaikh"); a.add("Asharf"); a.add("Husain");
+		 
+
+		// traversing list through iterato
+		Iterator itr = a.iterator();
+		while (itr.hasNext()) {// checks if iterartot has next element
+			System.out.println(itr.next());// printing the elements and move to next
+		}
+	}
+}
