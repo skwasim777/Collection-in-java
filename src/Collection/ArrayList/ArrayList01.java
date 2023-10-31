4706c53615e92ee90e09dfc836fef4bc605c0ced
skwasim777 <wasimsk6061@gmail.com>
2023-10-31 18:44:15 +0530

    arraylist first example

---
+++ src/Collection/ArrayList/ArrayList01.java
@@ -0,0 +1,17 @@
+package Collection.ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayList01 {
+	public static void main(String[] args) {
+
+		ArrayList<String> arr = new ArrayList<String>();
+		// adding objects in arraylist
+		arr.add("Mango");
+		arr.add("Apples");
+		arr.add("Banana");
+		arr.add("Graphs");
+		// print the arraylist objects
+		System.out.println(arr);
+	}
+}
